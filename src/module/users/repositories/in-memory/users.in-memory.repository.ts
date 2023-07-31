d4d8d2a4628b3ba49448cbf0d1aef688bd83ca87
samirdourado <samir3500@gmail.com>
2023-07-31 10:26:11 -0300

    Feat: Users CRUD in memory lógic

--- src/module/users/repositories/in-memory/users.in-memory.repository.ts
+++ src/module/users/repositories/in-memory/users.in-memory.repository.ts
@@ -1,3 +1,4 @@
+import { plainToInstance } from 'class-transformer';
 import { CreateUserDto } from '../../dto/create-user.dto';
 import { UpdateUserDto } from '../../dto/update-user.dto';
 import { User } from '../../entities/user.entity';
@@ -13,14 +14,14 @@ export class UsersInMemoryRepository implements UsersRepository {
 
     this.database.push(newUser);
 
-    return newUser;
+    return plainToInstance(User, newUser);
   }
   findAll(): User[] | Promise<User[]> {
-    return this.database;
+    return plainToInstance(User, this.database);
   }
   findOne(id: string): User | Promise<User> {
     const user = this.database.find((user) => user.id === id);
-    return user;
+    return plainToInstance(User, user);
   }
   update(id: string, data: UpdateUserDto): User | Promise<User> {
     const userIndex = this.database.findIndex((user) => user.id === id);
@@ -28,8 +29,7 @@ export class UsersInMemoryRepository implements UsersRepository {
       ...this.database[userIndex],
       ...data,
     };
-
-    return this.database[userIndex];
+    return plainToInstance(User, this.database[userIndex]);
   }
   delete(id: string): void | Promise<void> {
     const userIndex = this.database.findIndex((user) => user.id === id);
