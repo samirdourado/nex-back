402b97b4b06dc33788d9d4dc4e8944660af772f8
samirdourado <samir3500@gmail.com>
2023-07-31 00:00:03 -0300

    Feat: Product modules and crud in memory repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './module/users/users.module';
+import { ProductsModule } from './module/products/products.module';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, ProductsModule],
 })
 export class AppModule {}
--- src/module/users/repositories/in-memory/users.in-memory.repository.ts
+++ src/module/users/repositories/in-memory/users.in-memory.repository.ts
@@ -1,4 +1,5 @@
 import { CreateUserDto } from '../../dto/create-user.dto';
+import { UpdateUserDto } from '../../dto/update-user.dto';
 import { User } from '../../entities/user.entity';
 import { UsersRepository } from '../users.repository';
 
@@ -13,5 +14,25 @@ export class UsersInMemoryRepository implements UsersRepository {
     this.database.push(newUser);
 
     return newUser;
+  }
+  findAll(): User[] | Promise<User[]> {
+    return this.database;
+  }
+  findOne(id: string): User | Promise<User> {
+    const user = this.database.find((user) => user.id === id);
+    return user;
+  }
+  update(id: string, data: UpdateUserDto): User | Promise<User> {
+    const userIndex = this.database.findIndex((user) => user.id === id);
+    this.database[userIndex] = {
+      ...this.database[userIndex],
+      ...data,
+    };
+
+    return this.database[userIndex];
+  }
+  delete(id: string): void | Promise<void> {
+    const userIndex = this.database.findIndex((user) => user.id === id);
+    this.database.splice(userIndex, 1);
   }
 }
