3b28296407ff56f1845349e1584c993a533a0ab6
samirdourado <samir3500@gmail.com>
2023-07-30 22:02:23 -0300

    first commit

---
+++ src/module/users/entities/user.entity.ts
@@ -0,0 +1,13 @@
+import { randomUUID } from 'node:crypto';
+
+export class User {
+  readonly id: string;
+  name: string;
+  email: string;
+  password: string;
+  avatar: string;
+
+  constructor() {
+    this.id = randomUUID();
+  }
+}
